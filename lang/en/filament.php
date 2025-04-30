1a548265d4cc0547b38f927f73baffaa6c7c2c91
Ace Dinh <acedinh.contact@gmail.com>
2025-04-30 13:55:39 +0700

    add english lang

--- lang/en/filament.php
+++ lang/en/filament.php
@@ -3,4 +3,13 @@
 return [
     'set_role' => 'Set Role',
     'role' => 'Role',
+    'settings' => 'Settings',
+    'site_name' => 'Site Name',
+    'site_info' => 'Site Info',
+    'manage_setting' => 'Manage Setting',
+    'site_active' => 'Site Active',
+    'registration_enabled' => 'Registration Enabled',
+    'password_reset_enabled' => 'Password Reset Enabled',
+    'sso_enabled' => 'SSO Enabled',
+    'manage_profile' => 'Manage Profile',
 ];
